02ac5b798726b1f0106fe045b363343abef22f9e
nawnoes <nawnoes@gmail.com>
2020-10-27 20:50:01 +0900

    electra pretrain test

--- example/wordpiece_tokenizer.py
+++ example/wordpiece_tokenizer.py
@@ -7,6 +7,16 @@ vocab_path3 = "../data/wpm-vocab-all.txt"
 tokenizer = BertTokenizer(vocab_file=vocab_path3, do_lower_case=False)
 
 """  """
-print(tokenizer.decode(tokenizer.encode(' [CLS] 나는 워드피스 토크나이저를 써요. 예대마진이 요즘 많이 나오네요 [SEP] [CLS] 너는 바보인거  [SEP]',add_special_tokens=False)))
-print(tokenizer.encode('나는 워드피스 토크나이저를 써요. 예대마진이 요즘 많이 나오네요',add_special_tokens=False))
-print(tokenizer.decode(tokenizer.encode('[코로나 범정부 대응- 文 ‘서울시 방역’ 긴급 점검·3개 부처 대국민담화]')))
+test_str = ' [CLS] 나는 워드피스 토크나이저를 써요. 성능이 좋은지 테스트 해보려 합니다. [SEP]'
+print('테스트 문장: ',test_str)
+encoded_str = tokenizer.encode(test_str,add_special_tokens=False)
+print('문장 인코딩: ',encoded_str)
+decoded_str = tokenizer.decode(encoded_str)
+print('문장 디코딩: ',decoded_str)
+
+"""
+겱과
+테스트 문장:   [CLS] 나는 워드피스 토크나이저를 써요. 성능이 좋은지 테스트 해보려 합니다. [SEP]
+문장 인코딩:  [2, 9310, 4868, 6071, 12467, 21732, 12200, 6126, 6014, 4689, 6100, 18, 11612, 6037, 9389, 6073, 16784, 17316, 6070, 10316, 18, 3]
+문장 디코딩:  [CLS] 나는 워드피스 토크나이저를 써요. 성능이 좋은지 테스트 해보려 합니다. [SEP]
+"""
